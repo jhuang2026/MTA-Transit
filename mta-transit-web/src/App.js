ffe0acd021ff8b7a7f2d6cb9552867681fe8206b
zzrorn <j.huang7000@gmail.com>
2023-05-31 14:40:30 -0400

    https to axios

--- mta-transit-web/src/App.js
+++ mta-transit-web/src/App.js
@@ -1,45 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { api } from './api';
-
-const https = require('https');
-const GtfsRealtimeBindings = require('gtfs-realtime-bindings');
+import { fetchData } from './fetch.js';
 
 function App() {
   console.log("One");
-
-  const [feedEntities, setFeedEntities] = useState([]);
-
+  fetchData();
   console.log("Two");
-
-  useEffect(() => {
-    const fetchData = () => {
-      https.get(
-        `${api.base}/nyct%2Fgtfs-ace`,
-        { headers: { "x-api-key": api.key } },
-        (resp) => {
-          let data = [];
-
-          resp.on('data', (chunk) => {
-            data.push(chunk);
-          });
-
-          resp.on('end', () => {
-            const buffer = Buffer.concat(data);
-
-            // Parse the data from protobuf buffer
-            const feed = GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(buffer);
-
-            // Access the decoded message
-            console.log(feed);
-
-            setFeedEntities(feed.entity);
-          });
-        }
-      ).on("error", (err) => {
-        console.log("Error: " + err.message);
-      });
-    };
-  }, []);
 
   return (
     <div>

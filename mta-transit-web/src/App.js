7ad4ecb36eceb2ccc7aa12732ec515f45b633ca1
zzrorn <j.huang7000@gmail.com>
2023-05-31 15:49:43 -0400

    Can console log MTA data

--- mta-transit-web/src/App.js
+++ mta-transit-web/src/App.js
@@ -1,15 +1,46 @@
 import React, { useState, useEffect } from 'react';
-import { fetchData } from './fetch.js';
+import axios from 'axios';
+import { Buffer } from 'buffer';
+
+import {api} from './api.js';
+
+var GtfsRealtimeBindings = require('gtfs-realtime-bindings');
 
 function App() {
-  console.log("One");
-  fetchData();
-  console.log("Two");
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get(`${api.base}/nyct%2Fgtfs-ace`, {
+        headers: {
+          'x-api-key': api.key,
+        },
+        responseType: 'arraybuffer',
+      })
+
+      .then((response) => {
+        const buffer = Buffer.from(response.data, 'binary');
+
+        // Parse the data from protobuf buffer
+        const feed = GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(buffer);
+
+        // Access the decoded message from the feed
+        console.log(feed);
+        console.log('Receiving Data');
+        setData(feed.entity);
+      })
+
+      .catch((error) => {
+        console.log('Error: ' + error.message);
+      });
+  }, []);
 
   return (
     <div>
       <h1>MTA GTFS Realtime API</h1>
       <h2>Feed Entities</h2>
+      {data.map((entity, index) => (
+        <p key={index}>{JSON.stringify(entity)}</p>
+      ))}
     </div>
   );
 }
